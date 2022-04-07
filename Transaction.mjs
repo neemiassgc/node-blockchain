6d2fffe77af7856e3268cb7bfbec917a654a04e5
Neemias Santos <neemiassgc@outlook.com>
2022-04-07 04:29:11 +0100

    Create a file that defines a transaction

---
+++ Transaction.mjs
@@ -0,0 +1,14 @@
+class Transaction {
+    
+    constructor(amount, payer, payee) {
+        this.amount = amount;
+        this.payer = payer;
+        this.payee = payee;
+    }
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+export default Transaction;
